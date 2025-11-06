4d172e3bcfc5807dbff4abd3118667adcc31301d
arshadtp <arshadtp89@gmail.com>
2025-11-06 11:40:20 +0530

    Implement all C# operator examples (arithmetic, relational, logical, etc.)

--- Program.cs
+++ Program.cs
@@ -1,16 +1,94 @@
 ﻿
-int num1 = 10;
-long num2 = 12345;
-float num3 = 1.42f;
-double num4 = 2.234566;
-bool boolum = false;
-char A = 'b';
-string abc = "Arshad";
+// int num1 = 10;
+// long num2 = 12345;
+// float num3 = 1.42f;
+// double num4 = 2.234566;
+// bool boolum = false;
+// char A = 'b';
+// string abc = "Arshad";
 
-Console.WriteLine(num1);
-Console.WriteLine(num2);
-Console.WriteLine(num3);
-Console.WriteLine(num4);
-Console.WriteLine(boolum);
-Console.WriteLine(A);
-Console.WriteLine(abc);
+// Console.WriteLine(num1);
+// Console.WriteLine(num2);
+// Console.WriteLine(num3);
+// Console.WriteLine(num4);
+// Console.WriteLine(boolum);
+// Console.WriteLine(A);
+// Console.WriteLine(abc);
+
+
+
+// Arithmetic operation
+
+// int a = 30;
+// int b = 20;
+
+// Console.WriteLine(a + b);- 50
+// Console.WriteLine(a - b);- 10
+// Console.WriteLine(a * b);- 600
+// Console.WriteLine(a / b);- 1
+// Console.WriteLine(a % b);- 10
+
+// Relational 
+
+// int a = 30;
+// int b = 20;
+
+// Console.WriteLine(a < b);- false
+// Console.WriteLine(a > b);- true
+// Console.WriteLine(a <= b);- false
+// Console.WriteLine(a >= b);- true
+// Console.WriteLine(a == b);- false 
+// Console.WriteLine(a != b);- true          
+
+
+// Logical
+
+//  int a = 30;
+// int b = 20;
+
+// Console.WriteLine(a == 30 && b == 20);- true
+// Console.WriteLine(a == 10 || b == 20);- true
+// Console.WriteLine(!(a == 30));- false
+
+
+// Assignment
+
+// int a = 10;
+// Console.WriteLine(a); 10
+
+// int b = 20;
+// b += 10;
+// Console.WriteLine(b); 30
+
+// int c = 20;
+// c -= 10;
+// Console.WriteLine(c); 10
+
+// int d = 20;
+// d *= 10;
+// Console.WriteLine(d); 200
+
+// int e = 20;
+// e /= 10;
+// Console.WriteLine(e); 2
+
+
+
+// unary
+
+// int a = 10;
+// a++;
+// Console.WriteLine(a); 11
+
+// int a = 10;
+// a--;
+// Console.WriteLine(a); 9
+
+
+// Ternary
+
+// int a = 10;
+
+//  String result =a > 9 ? "positive number" : "Negative number";
+
+// Console.WriteLine(result);
